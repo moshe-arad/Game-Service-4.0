a14f53d97d990b07e227534efce192eb85531ea0
moshe-arad <moshe.arad.mailbox@gmail.com>
2017-06-24 19:38:55 +0300

    handling move on board

--- src/main/java/org/moshe/arad/backgammon/move/BackgammonBoardLocation.java
+++ src/main/java/org/moshe/arad/backgammon/move/BackgammonBoardLocation.java
@@ -1,8 +1,10 @@
 package org.moshe.arad.backgammon.move;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class BackgammonBoardLocation implements BoardLocation{
 
 	private Integer index;
